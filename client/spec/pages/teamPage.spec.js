3f7d6d8f9c9a5106a2a93de741a29ac142df0891
Nathan Urquhart <nathanu@AUnathanu.gateway>
2018-03-24 18:44:19 +1100

    [Added Team Space js and names in Team]

--- client/spec/pages/teamPage.spec.js
+++ client/spec/pages/teamPage.spec.js
@@ -1,17 +1,11 @@
 const TeamPage = require('../../src/js/pages/teamPage');
 
-describe('TeamPage', () => {
-  let watchFace;
-  beforeEach(() => {
-    document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
-    watchFace = document.getElementById('watch-face');
-  });
+let page;
 
-  describe('#template', () => {
-    it('should have a template', () => {
-      const page = new TeamPage();
-      expect(page.template()).toContain("<h1>Made by:</h1>");
+  describe('template', () => {
+    it('should contain the correct text', () => {
+
+      page = new TeamPage();
+      expect(page.template()).toContain('nathan', 'avengers');
     });
   });
-
-});
